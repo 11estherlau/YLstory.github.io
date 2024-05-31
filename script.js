69b222110885a6f75363ddbc370a86d27b607617
Esther <168502125+11estherlau@users.noreply.github.com>
2024-05-31 17:19:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ document.addEventListener('DOMContentLoaded', function() {
     navMenu.classList.toggle('collapsed');
   });
 });
+
+// Get menu and icon elements
+var menu = document.querySelector('.toggle-menu');
+var icon = document.querySelector('.toggle-icon');
+
+// Add a click event listener
+icon.addEventListener('click', function() {
+  if (menu.style.display === 'none') {
+    // Add a click event listener
+    menu.style.display = 'block';
+  } else {
+    // If the menu is displayed, hide the menu
+    menu.style.display = 'none';
+  }
+});
