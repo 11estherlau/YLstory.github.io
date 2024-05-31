c39e7817685d54f8d0d8467d4543a5ebdacff71a
Esther <168502125+11estherlau@users.noreply.github.com>
2024-05-31 17:42:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,5 @@ var icon = document.querySelector('.toggle-icon');
 
 // Add a click event listener
 icon.addEventListener('click', function() {
-  if (menu.style.display === 'none') {
-    // Add a click event listener
-    menu.style.display = 'block';
-  } else {
-    // If the menu is displayed, hide the menu
-    menu.style.display = 'none';
-  }
+  menu.classList.toggle('show-menu');
 });
